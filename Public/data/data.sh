be85bdbad12d94e06e608c7defc2f7b75f9bdc2d
Shin-junyeob <junyub029@gmail.com>
2025-07-08 07:37:31 +0000

    conf: Shell Script to Download data

---
+++ Public/data/data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# bash data.sh < do copy & paste. you can download data files.
+wget https://aistages-api-public-prod.s3.amazonaws.com/app/Competitions/000363/data/data.tar
+
+tar -xvf data.tar
